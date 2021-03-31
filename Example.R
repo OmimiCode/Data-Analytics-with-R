e601a105e03e9587b7b1b99b26deeabc3e37d3ec
Oluwanishola227639 <barnabas.oretan@gmail.com>
2021-03-31 17:00:53 +0100

    data analytics with R

---
+++ Example.R
@@ -0,0 +1,28 @@
+height = 50
+height
+
+people <- c("Esther", "Favor", "Ismail")
+people
+
+churches <- c("CAC", "TAC", "REDEEM", "WINNERS")
+churches
+length(churches)
+ont_to_ninety <- seq(1, 45, 3)
+ont_to_ninety
+
+colors <- c("Amber", "Turquoise", "Orangred", "Peach", "Blue")
+index <- c(2,3,5)
+colors[index]
+p <- matrix(nrow = 4, ncol = 5)
+p
+j <- matrix(1:50,5, nrow = 2,  ncol = 5)
+j
+x <- c(1,2,3,4,5,6,7,8,9,10)
+dim(x) <- c(2,5)
+x
+a <- c(7:10)
+b <- c(1:4)
+b_ <- rbind(a, b)
+b_
+a_ <- cbind(b, a)
+a_
